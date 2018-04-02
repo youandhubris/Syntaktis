05fd0f2b2fe6cfc294e91c1ff3ed2c534396112e
Hubris / Mac <yes@hubris.xyz>
2018-04-02 10:15:28 +0100

    repo transfer & gitignore

---
+++ AutoSave.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+using Hubris;
+
+namespace Hubris.EditorUtility
+{
+    /// <summary>
+    /// Saves active scene on Play.
+    /// </summary>
+    [InitializeOnLoadAttribute]
+    public static class SaveOnPlay
+    {
+        static SaveOnPlay()
+        {
+            EditorApplication.playModeStateChanged += DoAutoSaveOnPlay;
+        }
+
+        private static void DoAutoSaveOnPlay(PlayModeStateChange state)
+        {
+            switch (state)
+            {
+                case PlayModeStateChange.EnteredEditMode:
+                    break;
+                case PlayModeStateChange.ExitingEditMode:
+
+                    Scene activeScene = EditorSceneManager.GetActiveScene();
+
+                    if (activeScene.isDirty)
+                    {
+                        bool savedScene = EditorSceneManager.SaveScene(activeScene);
+                        if (savedScene) Debug.unityLogger.Log(typeof(SaveOnPlay).ToString(), "Active scene was saved.");
+
+                        AssetDatabase.SaveAssets();
+                    }
+                    break;
+                case PlayModeStateChange.EnteredPlayMode:
+                    break;
+                case PlayModeStateChange.ExitingPlayMode:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// This script creates a new window in the editor with a auto-save function. 
+    /// It's saving your current scene with a specified interval from 1 minute to 60 minutes.
+    /// The config window is on Window/Hubris/Auto-Save.
+    /// </summary>
+    public class AutoSaveOnEditor : EditorWindow
+    {
+        private bool autoSaveScene = true;
+        private int autoSaveInterval = 5;
+        private bool showLog = true;
+        private int lastAutoSaveTime = Khronos.UnixTimeNow();
+
+        [MenuItem("Hubris/Auto-Save", false, 1)]
+        static void Init()
+        {
+            var window = (AutoSaveOnEditor)GetWindow(typeof(AutoSaveOnEditor));
+            var content = new GUIContent
+            {
+                text = "Auto-Save",
+                image = Resources.Load("Hubris-Favicon") as Texture2D
+            };
+
+            window.titleContent = content;
+        }
+
+        public void Awake()
+        {
+            EditorApplication.update += Update;
+        }
+
+        public void OnDestroy()
+        {
+            EditorApplication.update -= Update;
+        }
+
+        void Update()
+        {
+            if (autoSaveScene && CheckTimeInterval())
+            {
+                SaveScene();
+                lastAutoSaveTime = Khronos.UnixTimeNow();
+            }
+         }
+
+        bool CheckTimeInterval()
+        {
+            if (Khronos.UnixTimeNow() >= (lastAutoSaveTime + autoSaveInterval * 60)) return true;
+            else return false;
+        }
+
+        void SaveScene()
+        {
+            Scene activeScene = EditorSceneManager.GetActiveScene();
+
+            if (activeScene.isDirty)
+            {
+                bool savedScene = EditorSceneManager.SaveScene(activeScene);
+                if (savedScene) Debug.unityLogger.Log(GetType().ToString(), "Active scene was auto-saved at " + Khronos.UnixTimeNow());
+
+                AssetDatabase.SaveAssets();
+            }
+        }
+
+        void OnGUI()
+        {
+            GUILayout.Label("Info:", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("Saving:", EditorSceneManager.GetActiveScene().name);
+            EditorGUILayout.Space();
+
+            GUILayout.Label("Options:", EditorStyles.boldLabel);
+            autoSaveScene = EditorGUILayout.BeginToggleGroup("Auto-Save", autoSaveScene);
+            showLog = EditorGUILayout.BeginToggleGroup("Log", showLog);
+            autoSaveInterval = EditorGUILayout.IntSlider("Interval (minutes)", autoSaveInterval, 1, 60);
+        }
+    }
+}
---
+++ Screenshot.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+using System;
+using System.IO;
+
+namespace Hubris.EditorUtility
+{
+    /// <summary>
+    /// </summary>
+    public static class Screenshot
+    {
+        [MenuItem("Hubris/Screenshot/Game %#q", false, 51)]
+        static void ScreenshotGame()
+        {
+            string path = Application.dataPath + "/StreamingAssets/";
+            string filename = GetFilename("Game");
+
+            if (!Directory.Exists(path))
+            {
+                try
+                {
+                    Directory.CreateDirectory(path);
+                }
+                catch (Exception e)
+                {
+                    Debug.unityLogger.LogError(typeof(SaveOnPlay).ToString(), e);
+                    throw;
+                }
+            }
+
+            ScreenCapture.CaptureScreenshot(path + filename);
+            Debug.unityLogger.Log(typeof(SaveOnPlay).ToString(), "Saved: " + path + filename);
+        }
+
+        [MenuItem("Hubris/Screenshot/Scene %#w", false, 52)]
+        static void ScreenshotSceneNoGizmos()
+        {
+            ScreenshotScene(false);
+        }
+
+        [MenuItem("Hubris/Screenshot/Scene + Gizmos %#e", false, 53)]
+        static void ScreenshotSceneGizmos()
+        {
+            ScreenshotScene(true);
+        }
+
+        static void ScreenshotScene(bool gizmos)
+        {
+            RenderTexture renderTexture = Camera.current.activeTexture;
+            if (!gizmos) RenderTexture.active = renderTexture;
+
+            Texture2D texture2d = new Texture2D(renderTexture.width, renderTexture.height);
+            texture2d.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+            texture2d.Apply();
+
+            byte[] bytes = texture2d.EncodeToPNG();
+            string path = Application.dataPath + "/StreamingAssets/";
+            string filename = GetFilename(gizmos ? "SceneGizmos" : "Scene");
+
+            if (SaveScreenshot(bytes, path, filename))
+            {
+                RenderTexture.active = null;
+                MonoBehaviour.DestroyImmediate(renderTexture);
+                MonoBehaviour.DestroyImmediate(texture2d);
+
+                Debug.unityLogger.Log(typeof(SaveOnPlay).ToString(), "Saved: " + path + filename);
+            }
+            else Debug.unityLogger.LogError(typeof(SaveOnPlay).ToString(), "Not saved.");
+
+        }
+
+        static string GetFilename(string type)
+        {
+            return EditorSceneManager.GetActiveScene().name + "-" + type + "-" + Khronos.GetDateCompact(DateTime.Now) + ".png";
+        }
+
+        static bool SaveScreenshot(byte [] bytes, string path, string filename)
+        {
+            bool saved = false;
+
+            if (!Directory.Exists(path))
+            {
+                try
+                {
+                    Directory.CreateDirectory(path);
+                }
+                catch (Exception e)
+                {
+                    Debug.unityLogger.LogError(typeof(SaveOnPlay).ToString(), e);
+                    throw;
+                }
+            }
+
+            try
+            {
+                File.WriteAllBytes(path + filename, bytes);
+                saved = true;
+            }
+            catch (Exception e)
+            {
+                Debug.unityLogger.LogError(typeof(SaveOnPlay).ToString(), e);
+                throw;
+            }
+
+            return saved;
+        }
+
+    }
+}
